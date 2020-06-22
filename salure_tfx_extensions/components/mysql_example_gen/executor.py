ee88e038f23336a070832a03adbbc325e46a1d62
nielsgroeneveld <groeneveld@salure.nl>
2020-06-22 16:14:55 +0200

    Disable MySQLExampleGen logging of rows

--- salure_tfx_extensions/components/mysql_example_gen/executor.py
+++ salure_tfx_extensions/components/mysql_example_gen/executor.py
@@ -104,7 +104,7 @@ class _ReadMySQLDoFn(beam.DoFn):
                         a[col_name] = field
                     # TODO: this not the right output (most likely, since examples arriving at transform are empty)
                     # TODO: change output to column_name -> value dict
-                    print(a)
+                    # print(a)
                     yield a
             except:
                 raise
