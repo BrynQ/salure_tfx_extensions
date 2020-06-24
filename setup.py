b41f71c60f774e028b04d81912eb7d518009732d
nielsgroeneveld <groeneveld@salure.nl>
2020-06-24 17:49:29 +0200

    Fixed failing install

--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
 from setuptools import setup, find_packages
+from salure_tfx_extensions import version
 
-
-def read_key_value_pairs_from_file(filename):
-    result = {}
-    with open(filename) as f:
-        for line in f:
-            if line[0] != '#':
-                key, _, value = line.partition('=')
-                result[key.strip()] = value.strip()
-    return result
-
-
-version_variables = read_key_value_pairs_from_file('VERSION')
 
 with open('README.md') as f:
     long_description = f.read()
@@ -19,9 +8,9 @@ with open('README.md') as f:
 setup(
     name='salure_tfx_extensions',
     version='{}.{}.{}'.format(
-        version_variables["VERSION_MAJOR"],
-        version_variables["VERSION_MINOR"],
-        version_variables["VERSION_HOTFIX"]),
+        version.VERSION_MAJOR,
+        version.VERSION_MINOR,
+        version.VERSION_PATCH),
     description='TFX components, helper functions and pipeline definition, developed by Salure',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -31,7 +20,7 @@ setup(
     packages=find_packages(),
     package_data={'salure_tfx_extensions': ['proto/*.proto']},
     install_requires=[
-        'tfx>={}'.format(version_variables["TFX_VERSION"]),
+        'tfx>={}'.format(version.TFX_VERSION),
         # 'tensorflow>=1.15.0',
         # 'beam-nuggets>=0.15.1,<0.16',
         'PyMySQL>=0.9.3,<0.10'
