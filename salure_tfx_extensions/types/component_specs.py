9cf1e130a603d359dcdc08523ece5859f68a7544
nielsgroeneveld <groeneveld@salure.nl>
2020-06-25 12:18:31 +0200

    Updated BaseComponent to also reflect its updated ComponentSpec

--- salure_tfx_extensions/types/component_specs.py
+++ salure_tfx_extensions/types/component_specs.py
@@ -1,14 +1,18 @@
 """Standard Component specs for the salure_tfx_extensions library"""
 
 from tfx.types import ComponentSpec
-from tfx.types.component_spec import ChannelParameter
+from tfx.types.component_spec import ChannelParameter, ExecutionParameter
 from tfx.types import standard_artifacts
+from tfx.proto import example_gen_pb2
 
 
 class BaseSpec(ComponentSpec):
     """Salure_tfx_extensions BaseComponent spec"""
 
-    PARAMETERS = {}
+    PARAMETERS = {
+        'input_config': ExecutionParameter(type=example_gen_pb2.Input),
+        'output_config': ExecutionParameter(type=example_gen_pb2.Output),
+    }
     INPUTS = {
         'examples': ChannelParameter(type=standard_artifacts.Examples)
     }
