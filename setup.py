9ffecb2b96db8d6d62e17123ca133eb18d8ad23e
Niels Groeneveld <groeneveld@salure.nl>
2020-07-23 12:08:06 +0200

    Started Finishing up SKLearnTransform

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # IMPORTANT: ALSO UPDATE IN 'VERSION' FILE FOR CI DOCKER BUILD
 VERSION_MAJOR = '0'
 VERSION_MINOR = '0'
-VERSION_PATCH = '77'
+VERSION_PATCH = '78'
 
 
 with open('README.md') as f:
