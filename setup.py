c95179c08d7e507661be0a014075b29e2ba94042
Niels Groeneveld <groeneveld@salure.nl>
2020-08-19 16:39:40 +0200

    Started on SeldonDeployment Integration

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # IMPORTANT: ALSO UPDATE IN 'VERSION' FILE FOR CI DOCKER BUILD
 VERSION_MAJOR = '0'
 VERSION_MINOR = '0'
-VERSION_PATCH = '79'
+VERSION_PATCH = '80'
 
 
 with open('README.md') as f:
