698d1afadc51ea2ecabdf59d9400b2838638c68c
Niels Groeneveld <groeneveld@salure.nl>
2021-02-05 14:19:11 +0100

    debugging protobuf parsing

--- salure_tfx_extensions/components/mysql_pusher/executor.py
+++ salure_tfx_extensions/components/mysql_pusher/executor.py
@@ -75,7 +75,8 @@ class Executor(base_executor.BaseExecutor):
                     | 'ReadPredictionLogs' >> beam.io.ReadFromTFRecord(
                         predictions_uri,
                         coder=beam.coders.ProtoCoder(prediction_log_pb2.PredictionLog))
-                    | 'ParsePredictionLogs' >> beam.Map(parse_predictlog))
+                    # | 'ParsePredictionLogs' >> beam.Map(parse_predictlog))
+                    | 'ParsePredictionLogs' >> beam.Map(protobuf_to_dict))
 
             _ = (data
                  | 'Log PredictionLogs' >> beam.Map(absl.logging.info))
@@ -158,6 +159,78 @@ def parse_predictlog(predict_log):
     example = tf.train.Example.FromString(example)
 
     return example, predict_val
+
+
+
+# protobuf_to_dict is from https://github.com/benhodgson/protobuf-to-dict
+from google.protobuf.message import Message
+from google.protobuf.descriptor import FieldDescriptor
+
+EXTENSION_CONTAINER = '___X'
+
+
+TYPE_CALLABLE_MAP = {
+    FieldDescriptor.TYPE_DOUBLE: float,
+    FieldDescriptor.TYPE_FLOAT: float,
+    FieldDescriptor.TYPE_INT32: int,
+    FieldDescriptor.TYPE_INT64: int,
+    FieldDescriptor.TYPE_UINT32: int,
+    FieldDescriptor.TYPE_UINT64: int,
+    FieldDescriptor.TYPE_SINT32: int,
+    FieldDescriptor.TYPE_SINT64: int,
+    FieldDescriptor.TYPE_FIXED32: int,
+    FieldDescriptor.TYPE_FIXED64: int,
+    FieldDescriptor.TYPE_SFIXED32: int,
+    FieldDescriptor.TYPE_SFIXED64: int,
+    FieldDescriptor.TYPE_BOOL: bool,
+    FieldDescriptor.TYPE_STRING: str,
+    FieldDescriptor.TYPE_BYTES: lambda b: b.encode("base64"),
+    FieldDescriptor.TYPE_ENUM: int,
+}
+
+
+def protobuf_to_dict(pb, type_callable_map=TYPE_CALLABLE_MAP, use_enum_labels=False):
+    result_dict = {}
+    extensions = {}
+    for field, value in pb.ListFields():
+        type_callable = _get_field_value_adaptor(pb, field, type_callable_map, use_enum_labels)
+        if field.label == FieldDescriptor.LABEL_REPEATED:
+            type_callable = repeated(type_callable)
+
+        if field.is_extension:
+            extensions[str(field.number)] = type_callable(value)
+            continue
+
+        result_dict[field.name] = type_callable(value)
+
+    if extensions:
+        result_dict[EXTENSION_CONTAINER] = extensions
+    return result_dict
+
+def repeated(type_callable):
+    return lambda value_list: [type_callable(value) for value in value_list]
+
+
+def enum_label_name(field, value):
+    return field.enum_type.values_by_number[int(value)].name
+
+
+def _get_field_value_adaptor(pb, field, type_callable_map=TYPE_CALLABLE_MAP, use_enum_labels=False):
+    if field.type == FieldDescriptor.TYPE_MESSAGE:
+        # recursively encode protobuf sub-message
+        return lambda pb: protobuf_to_dict(pb,
+            type_callable_map=type_callable_map,
+            use_enum_labels=use_enum_labels)
+
+    if use_enum_labels and field.type == FieldDescriptor.TYPE_ENUM:
+        return lambda value: enum_label_name(field, value)
+
+    if field.type in type_callable_map:
+        return type_callable_map[field.type]
+
+    raise TypeError("Field %s.%s has unrecognised type id %d" % (
+        pb.__class__.__name__, field.name, field.type))
+
 
 
 
