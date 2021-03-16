e420b9d6193863864e8f119b26af96ec81609ea9
Xueming Li <li@salure.nl>
2021-03-16 14:13:45 +0100

    v0.0.153 Adjust python to be 3.6 and pandas <1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # IMPORTANT: ALSO UPDATE IN 'VERSION' FILE FOR CI DOCKER BUILD
 VERSION_MAJOR = '0'
 VERSION_MINOR = '0'
-VERSION_PATCH = '152'
+VERSION_PATCH = '153'
 
 
 with open('README.md') as f:
@@ -15,7 +15,7 @@ setup(
         VERSION_MAJOR,
         VERSION_MINOR,
         VERSION_PATCH),
-    python_requires='>=3.7',
+    python_requires='>=3.6',
     description='TFX components, helper functions and pipeline definition, developed by Salure',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -30,7 +30,7 @@ setup(
         # 'tensorflow-datasets>=3.0,<4.0',
         'tfx>=0.22.0,<0.23.0',
         # 'tensorflow==2.4.0',
-        'pandas>=0.25,<2',
+        'pandas<1,>=0.24',
         'PyMySQL>=0.9.3,<0.10',
         'kfp>=0.4,<0.5',
     ],
