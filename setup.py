1bd5290f2b800cc995973c067764d9519ba6aaad
Xueming Li <li@salure.nl>
2021-05-17 23:42:53 +0200

    v0.1.13 fix: Import StringIO in SQL_CSV_GEN executor

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # IMPORTANT: ALSO UPDATE IN 'VERSION' FILE FOR CI DOCKER BUILD
 VERSION_MAJOR = '0'
 VERSION_MINOR = '1'
-VERSION_PATCH = '12'
+VERSION_PATCH = '13'
 
 
 with open('README.md') as f:
