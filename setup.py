4bac276155ca71cd184bd20a27d57ae8f6722041
Xueming Li <li@salure.nl>
2021-12-29 18:56:03 +0100

    v0.2.11 fix: version conflicts

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # IMPORTANT: ALSO UPDATE IN 'VERSION' FILE FOR CI DOCKER BUILD
 VERSION_MAJOR = '0'
 VERSION_MINOR = '2'
-VERSION_PATCH = '10'
+VERSION_PATCH = '11'
 
 
 with open('README.md') as f:
@@ -23,7 +23,6 @@ setup(
     package_data={'salure_tfx_extensions': ['proto/*.proto']},
     install_requires=[
         'beam-nuggets>=0.15.1,<0.16',
-        'tensorflow==2.6.0',
         'tfx>=0.27.0,<0.28.0',
         'pandas>=1,<=1.35',
         'kfp>=0.4,<0.5',
